195c79c6d79608f445b88ba0b6f1f218aaa4435b
Siddharth Joisar <127229870+sidj01sar26@users.noreply.github.com>
2025-03-04 21:09:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 
-// database connection
+const app = express();
+
+// Connect to database
 connection();
 
-// middlewares
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
-// routes
+// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 8080;
-app.listen(port, console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
