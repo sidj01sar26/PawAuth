2e0c2dc782690303b11c84d1ef331def1691444d
Siddharth Joisar <127229870+sidj01sar26@users.noreply.github.com>
2025-03-04 21:12:32 +0530

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,26 +2,24 @@ const router = require("express").Router();
 const { User, validate } = require("../models/user");
 const bcrypt = require("bcrypt");
 
-router.post("/", async (req, res) => {
-	try {
-		const { error } = validate(req.body);
-		if (error)
-			return res.status(400).send({ message: error.details[0].message });
+router.post("/signup", async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send({ message: error.details[0].message });
 
-		const user = await User.findOne({ email: req.body.email });
-		if (user)
-			return res
-				.status(409)
-				.send({ message: "User with given email already Exist!" });
+        let user = await User.findOne({ email: req.body.email });
+        if (user) return res.status(409).send({ message: "User with this email already exists!" });
 
-		const salt = await bcrypt.genSalt(Number(process.env.SALT));
-		const hashPassword = await bcrypt.hash(req.body.password, salt);
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-		await new User({ ...req.body, password: hashPassword }).save();
-		res.status(201).send({ message: "User created successfully" });
-	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
-	}
+        user = new User({ ...req.body, password: hashPassword });
+        await user.save();
+
+        res.status(201).send({ message: "User created successfully" });
+    } catch (error) {
+        res.status(500).send({ message: "Internal Server Error" });
+    }
 });
 
 module.exports = router;
