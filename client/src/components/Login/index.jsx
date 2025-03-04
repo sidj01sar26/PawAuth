a91db130c3915345368364acf57f9df857e7f548
Siddharth Joisar <127229870+sidj01sar26@users.noreply.github.com>
2025-03-04 19:27:41 +0530

    Update index.jsx

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -14,7 +14,8 @@ const Login = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:8080/api/auth";
+			// const url = "http://localhost:8080/api/auth";
+			const url = "https://pawauth-backend.onrender.com";
 			const { data: res } = await axios.post(url, data);
 			localStorage.setItem("token", res.data);
 			window.location = "https://pawhelp.vercel.app/";
