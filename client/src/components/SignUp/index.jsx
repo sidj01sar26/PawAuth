734bd74b043816a39478a1e3dbb1b2d7cd5b89b7
Siddharth Joisar <127229870+sidj01sar26@users.noreply.github.com>
2025-03-04 19:28:57 +0530

    Update index.jsx

--- client/src/components/SignUp/index.jsx
+++ client/src/components/SignUp/index.jsx
@@ -20,7 +20,8 @@ const Signup = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:8080/api/users";
+			// const url = "http://localhost:8080/api/users";
+			const url = "https://pawauth-backend.onrender.com";
 			const { data: res } = await axios.post(url, data);
 			navigate("/login");
 			console.log(res.message);
